f0bb5a2e98311086fa691ea2a03a512afe382bc3
David French <thedonquixotic@gmail.com>
2015-11-08 02:45:05 -0600

    Linked buttons together and bough UI Builder

---
+++ TaskMonsterUIPrototype/Assets/button.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class button : MonoBehaviour {
+public void PlayButton (int scene)
+	{
+		Application.LoadLevel (scene);
+	}
+
+public void ExitButton () {
+
+	Application.Quit ();
+}
+}
